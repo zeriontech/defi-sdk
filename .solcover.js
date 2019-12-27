5d8d36fe1adfd1eda3f517234e200133f582cb16
Igor Sobolev <sobolev.igor@me.com>
2019-12-28 01:30:30 +0300

    updated configs

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,10 @@
 module.exports = {
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    norpc: true,
-    skipFiles: [
-        'Migrations.sol',
-        'ProtocolWrapperMock.sol',
-    ],
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+  norpc: true,
+  skipFiles: [
+    'Migrations.sol',
+    'ProtocolWrapperMock.sol',
+    'Structs.sol'
+  ],
 };
