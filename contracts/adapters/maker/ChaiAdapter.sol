e4ade985128f06f7dc59ebfa99df7f8a637f45e1
Igor Sobolev <sobolev.igor@me.com>
2020-03-18 13:33:14 +0300

    added chai adapter

---
+++ contracts/adapters/maker/ChaiAdapter.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.6.4;
+pragma experimental ABIEncoderV2;
+
+import { ProtocolAdapter } from "../ProtocolAdapter.sol";
+import { ERC20 } from "../../ERC20.sol";
+
+
+/**
+ * @title Adapter for Chai protocol.
+ * @dev Implementation of ProtocolAdapter interface.
+ */
+contract ChaiAdapter is ProtocolAdapter {
+
+    /**
+     * @return Type of the adapter.
+     * @dev Implementation of ProtocolAdapter interface function.
+     */
+    function adapterType() external pure override returns (string memory) {
+        return "Asset";
+    }
+
+    /**
+     * @return Type of the token used in adapter.
+     * @dev Implementation of ProtocolAdapter interface function.
+     */
+    function tokenType() external pure override returns (string memory) {
+        return "Chai token";
+    }
+
+    /**
+     * @return Amount of Chai tokens held by the given account.
+     * @dev Implementation of ProtocolAdapter interface function.
+     */
+    function getBalance(address token, address account) external view override returns (uint256) {
+        return ERC20(token).balanceOf(account);
+    }
+}
