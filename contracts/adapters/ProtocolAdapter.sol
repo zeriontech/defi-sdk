fd036c264ab09ca4d71a07ab69c07266008c4e80
Igor Sobolev <sobolev.igor@me.com>
2020-03-25 16:30:59 +0300

    updated copyright

--- contracts/adapters/ProtocolAdapter.sol
+++ contracts/adapters/ProtocolAdapter.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Igor Sobolev <sobolev@zerion.io>
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
