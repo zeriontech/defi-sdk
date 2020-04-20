3da21f66914e0b8c02440171c5e162a1d65d0dca
Igor Sobolev <sobolev.igor@me.com>
2020-04-20 13:45:02 +0300

    script for 1 new adapter

---
+++ migrations_scripts/1_use_registry_and_add_deployed_adapter.js
@@ -0,0 +1,48 @@
+const AdapterRegistry = artifacts.require('AdapterRegistry');
+
+let protocolNames = [];
+let metadata = [];
+let adapters = [];
+let tokens = [];
+let tokenAdapters = [];
+
+module.exports = async (deployer, network, accounts) => {
+  adapters.push(['']);
+  tokens.push([dmmAssetAdapterTokens]);
+  protocolNames.push('Initial protocol name');
+  metadata.push([
+    'Name',
+    'Description',
+    'WebsiteURL',
+    'IconURL',
+    '0',
+  ]);
+
+  tokenAdapters.push(
+    '',
+  );
+
+  await AdapterRegistry.at('0x06FE76B2f432fdfEcAEf1a7d4f6C3d41B5861672')
+    .then(async (registry) => {
+      await registry.contract.methods.addProtocols(
+        protocolNames,
+        metadata,
+        adapters,
+        tokens,
+      )
+        .send({
+          from: accounts[0],
+          gasLimit: '5000000',
+        });
+      await registry.contract.methods.addTokenAdapters(
+        [
+          'Initial token name',
+        ],
+        tokenAdapters,
+      )
+        .send({
+          from: accounts[0],
+          gasLimit: '5000000',
+        });
+    });
+};
