eff7dfb069f877575903c8ee1458dd40441e1e65
Igor Sobolev <sobolev@smartdec.ru>
2020-06-24 14:57:18 +0500

    synthetix adapter update

--- test/SynthetixAssetAdapter.js
+++ test/SynthetixAssetAdapter.js
@@ -9,6 +9,7 @@ contract('SynthetixAssetAdapter', () => {
   const sethUniAddress = '0xe9Cf7887b93150D4F2Da7dFc6D502B216438F244';
   const curveSnxAddress = '0xC25a3A3b969415c80451098fa907EC722572917F';
   const iETHAddress = '0xA9859874e1743A32409f75bB11549892138BBA1E';
+  const sxauUniAddress = '0x34a0216C5057bC18e5d34D4405284564eFd759b2';
   const testAddress = '0x42b9dF65B219B3dD36FF330A4dD8f327A6Ada990';
 
   let accounts;
@@ -21,24 +22,24 @@ contract('SynthetixAssetAdapter', () => {
     'SNX',
     '18',
   ];
-  // const sethUni = [
-  //   sethUniAddress,
-  //   'sETH pool',
-  //   'UNI-V1',
-  //   '18',
-  // ];
   const sethUni = [
     sethUniAddress,
     'Not available',
     'N/A',
     '0',
   ];
-  // const ieth = [
-  //   iETHAddress,
-  //   'Synth iETH',
-  //   'iETH',
-  //   '18',
-  // ];
+  const ieth = [
+    iETHAddress,
+    'Synth iETH',
+    'iETH',
+    '18',
+  ];
+  const sxauUni = [
+    sxauUniAddress,
+    'Uniswap V2',
+    'UNI-V2',
+    '18',
+  ];
   const curveSusd = [
     curveSnxAddress,
     'Curve.fi DAI/USDC/USDT/sUSD',
@@ -77,6 +78,7 @@ contract('SynthetixAssetAdapter', () => {
         sethUniAddress,
         curveSnxAddress,
         iETHAddress,
+        sxauUniAddress,
       ]]],
     )
       .send({
@@ -106,9 +108,12 @@ contract('SynthetixAssetAdapter', () => {
         displayToken(result[0].adapterBalances[0].balances[2].base);
         assert.deepEqual(result[0].adapterBalances[0].balances[2].base.metadata, curveSusd);
         assert.equal(result[0].adapterBalances[0].balances[2].underlying.length, 0);
-        // displayToken(result[0].adapterBalances[0].balances[3].base);
-        // assert.deepEqual(result[0].adapterBalances[0].balances[3].base.metadata, ieth);
-        // assert.equal(result[0].adapterBalances[0].balances[3].underlying.length, 0);
+        displayToken(result[0].adapterBalances[0].balances[3].base);
+        assert.deepEqual(result[0].adapterBalances[0].balances[3].base.metadata, ieth);
+        assert.equal(result[0].adapterBalances[0].balances[3].underlying.length, 0);
+        displayToken(result[0].adapterBalances[0].balances[4].base);
+        assert.deepEqual(result[0].adapterBalances[0].balances[4].base.metadata, sxauUni);
+        assert.equal(result[0].adapterBalances[0].balances[4].underlying.length, 0);
       });
   });
 });
