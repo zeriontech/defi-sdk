b4ee885b4a5e5d0e057dc12948de6655941b15ad
Igor Sobolev <sobolev@smartdec.ru>
2020-06-25 15:58:01 +0500

    synthetix test update

--- test/SynthetixAssetAdapter.js
+++ test/SynthetixAssetAdapter.js
@@ -8,6 +8,7 @@ contract('SynthetixAssetAdapter', () => {
   const snxAddress = '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F';
   const sethUniAddress = '0xe9Cf7887b93150D4F2Da7dFc6D502B216438F244';
   const curveSnxAddress = '0xC25a3A3b969415c80451098fa907EC722572917F';
+  const curveSbtcAddress = '0x075b1bb99792c9E1041bA13afEf80C91a1e70fB3';
   const iETHAddress = '0xA9859874e1743A32409f75bB11549892138BBA1E';
   const sxauUniAddress = '0x34a0216C5057bC18e5d34D4405284564eFd759b2';
   const testAddress = '0x42b9dF65B219B3dD36FF330A4dD8f327A6Ada990';
@@ -46,6 +47,12 @@ contract('SynthetixAssetAdapter', () => {
     'crvPlain3andSUSD',
     '18',
   ];
+  const curveSbtc = [
+    curveSbtcAddress,
+    'Curve.fi renBTC/wBTC/sBTC',
+    'crvRenWSBTC',
+    '18',
+  ];
 
   beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
@@ -79,6 +86,7 @@ contract('SynthetixAssetAdapter', () => {
         curveSnxAddress,
         iETHAddress,
         sxauUniAddress,
+        curveSbtcAddress,
       ]]],
     )
       .send({
@@ -114,6 +122,9 @@ contract('SynthetixAssetAdapter', () => {
         displayToken(result[0].adapterBalances[0].balances[4].base);
         assert.deepEqual(result[0].adapterBalances[0].balances[4].base.metadata, sxauUni);
         assert.equal(result[0].adapterBalances[0].balances[4].underlying.length, 0);
+        displayToken(result[0].adapterBalances[0].balances[5].base);
+        assert.deepEqual(result[0].adapterBalances[0].balances[5].base.metadata, curveSbtc);
+        assert.equal(result[0].adapterBalances[0].balances[5].underlying.length, 0);
       });
   });
 });
