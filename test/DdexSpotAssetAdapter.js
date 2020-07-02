6963e59a1e2fd21a929b397f3efd2de95c890fe1
Igor Sobolev <sobolev@smartdec.ru>
2020-07-02 13:55:32 +0500

    added ddex adapters and tests

---
+++ test/DdexSpotAssetAdapter.js
@@ -0,0 +1,76 @@
+import displayToken from './helpers/displayToken';
+
+const AdapterRegistry = artifacts.require('AdapterRegistry');
+const ProtocolAdapter = artifacts.require('DdexSpotAssetAdapter');
+const ERC20TokenAdapter = artifacts.require('ERC20TokenAdapter');
+
+contract('DdexSpotAssetAdapter', () => {
+  const ethAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+  const testAddress = '0x42b9dF65B219B3dD36FF330A4dD8f327A6Ada990';
+
+  let accounts;
+  let adapterRegistry;
+  let protocolAdapterAddress;
+  let erc20TokenAdapterAddress;
+  const dai = [
+    daiAddress,
+    'Dai Stablecoin',
+    'DAI',
+    '18',
+  ];
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    await ProtocolAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        protocolAdapterAddress = result.address;
+      });
+    await ERC20TokenAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        erc20TokenAdapterAddress = result.address;
+      });
+    await AdapterRegistry.new({ from: accounts[0] })
+      .then((result) => {
+        adapterRegistry = result.contract;
+      });
+    await adapterRegistry.methods.addProtocols(
+      ['Ddex • Spot'],
+      [[
+        'Mock Protocol Name',
+        'Mock protocol description',
+        'Mock website',
+        'Mock icon',
+        '0',
+      ]],
+      [[
+        protocolAdapterAddress,
+      ]],
+      [[[
+        ethAddress,
+        daiAddress,
+      ]]],
+    )
+      .send({
+        from: accounts[0],
+        gasLimit: '1000000',
+      });
+    await adapterRegistry.methods.addTokenAdapters(
+      ['ERC20'],
+      [erc20TokenAdapterAddress],
+    )
+      .send({
+        from: accounts[0],
+        gasLimit: '300000',
+      });
+  });
+
+  it('should return correct balances', async () => {
+    await adapterRegistry.methods['getBalances(address)'](testAddress)
+      .call()
+      .then((result) => {
+        displayToken(result[0].adapterBalances[0].balances[0].base);
+        assert.deepEqual(result[0].adapterBalances[0].balances[0].base.metadata, dai);
+      });
+  });
+});
