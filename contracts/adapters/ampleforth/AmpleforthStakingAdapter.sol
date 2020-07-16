8c3435e06d54d3c5741a958d7d18c6181c948d88
Igor Sobolev <sobolev.igor@me.com>
2020-07-16 18:06:30 +0700

    added ampl adapter

---
+++ contracts/adapters/ampleforth/AmpleforthStakingAdapter.sol
@@ -0,0 +1,68 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+import { ProtocolAdapter } from "../ProtocolAdapter.sol";
+
+
+/**
+ * @dev TokenGeyser contract interface.
+ * Only the functions required for AmpleforthStakingAdapter contract are added.
+ * The TokenGeyser contract is available here
+ * github.com/ampleforth/token-geyser/blob/master/contracts/TokenGeyser.sol.
+ */
+interface TokenGeyser {
+    function totalStakedFor(address) external view returns (uint256);
+}
+
+
+/**
+ * @title Asset adapter for Ampleforth Staking.
+ * @dev Implementation of ProtocolAdapter interface.
+ * @author Igor Sobolev <sobolev@zerion.io>
+ */
+contract AmpleforthStakingAdapter is ProtocolAdapter {
+
+    address internal immutable geyser_;
+
+    constructor(
+        address geyser
+    )
+        public
+    {
+        require(geyser != address(0), "ASA: empty geyser!");
+        geyser_ = geyser;
+    }
+
+    /**
+     * @return Amount of UNI-tokens locked on the protocol by the given account.
+     * @dev Implementation of ProtocolAdapter interface function.
+     */
+    function getBalance(
+        address,
+        address account
+    )
+        public
+        view
+        override
+        returns (uint256, bytes32)
+    {
+        TokenGeyser(geyser_).totalStakedFor(account);
+    }
+}
---
+++ test/adapters/AmpleforthAssetAdapter.js
@@ -0,0 +1,66 @@
+import displayToken from './helpers/displayToken';
+
+const AdapterRegistry = artifacts.require('AdapterRegistry');
+const ProtocolAdapter = artifacts.require('AmpleforthAssetAdapter');
+const ERC20TokenAdapter = artifacts.require('ERC20TokenAdapter');
+
+contract('AmpleforthAssetAdapter', () => {
+  const amplAddress = '0xD46bA6D942050d489DBd938a2C909A5d5039A161';
+  const testAddress = '0x42b9dF65B219B3dD36FF330A4dD8f327A6Ada990';
+
+  let accounts;
+  let adapterRegistry;
+  let protocolAdapterAddress;
+  let erc20TokenAdapterAddress;
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    await ProtocolAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        protocolAdapterAddress = result.address;
+      });
+    await ERC20TokenAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        erc20TokenAdapterAddress = result.address;
+      });
+    await AdapterRegistry.new({ from: accounts[0] })
+      .then((result) => {
+        adapterRegistry = result.contract;
+      });
+    await adapterRegistry.methods.addProtocols(
+      [
+        `${web3.eth.abi.encodeParameter(
+          'bytes32',
+          web3.utils.toHex('Aave'),
+        )
+          .slice(0, -2)}${ASSET_ADAPTER}`,
+      ],
+      [
+        protocolAdapterAddress,
+      ],
+      [[
+        amplAddress,
+      ]],
+    )
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+    await adapterRegistry.methods.addTokenAdapters(
+      ['ERC20'],
+      [erc20TokenAdapterAddress],
+    )
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+  });
+
+  it('should return correct balances', async () => {
+    await adapterRegistry.methods['getBalances(address)'](testAddress)
+      .call()
+      .then(async (result) => {
+        await displayToken(adapterRegistry, result[0].tokenBalances[0]);
+      });
+  });
+});
