1ff69a90ef98962e8023707bf4eb23dfc32c4d2a
Igor Sobolev <sobolev.igor@me.com>
2020-08-10 21:46:26 +0700

    fixed yfi adapter's tests

--- test/YearnStakingV1Adapter.js
+++ test/YearnStakingV1Adapter.js
@@ -1,45 +1,23 @@
 import displayToken from './helpers/displayToken';
 
 const AdapterRegistry = artifacts.require('AdapterRegistry');
-const ProtocolAdapter = artifacts.require('YFIAdapter');
-const TokenAdapter = artifacts.require('TokenSetsTokenAdapter');
+const ProtocolAdapter = artifacts.require('YearnStakingV1Adapter');
 const ERC20TokenAdapter = artifacts.require('ERC20TokenAdapter');
 
 
-contract('YFIAdapter', () => {
-  const yfi = '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e';
-  const yCrvAddress = '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8';
-  const balancerDai98Yfi2Address = '0x60626db611a9957C1ae4Ac5b7eDE69e24A3B76c5';
-  const balancerYfi2yCrv98Address = '0x95C4B6C7CfF608c0CA048df8b81a484aA377172B';
+contract.only('YearnStakingV1Adapter', () => {
+  const yfiAddress = '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e';
+  const testAddress = '0xCa845A71d1ff53E7DB7769Ae3f356AF53Fb43000';
 
   let accounts;
   let adapterRegistry;
   let protocolAdapterAddress;
-  let tokenAdapterAddress;
   let erc20TokenAdapterAddress;
   const yfi = [
     yfiAddress,
     'yearn.finance',
     'YFI',
     '18',
-  ];
-  const yCrv = [
-    yCrvAddress,
-    'Curve.fi',
-    'yDAI+yUSDC+yUSDT+yTUSD',
-    '18',
-  ];
-  const balancerDai98Yfi2 = [
-    balancerDai98Yfi2Address,
-    'Not available',
-    'N/A',
-    '0',
-  ];
-  const balancer_YFI_CURVE_Y_2_98 = [
-    balancer_YFI_CURVE_Y_2_98,
-    'Not available',
-    'N/A',
-    '0',
   ];
 
   beforeEach(async () => {
@@ -70,9 +48,6 @@ contract('YFIAdapter', () => {
       ]],
       [[[
         yfi,
-        yCrv,
-        balancerDai98Yfi2,
-        balancerYfi2yCrv98,
       ]]],
     )
       .send({
@@ -96,15 +71,6 @@ contract('YFIAdapter', () => {
         displayToken(result[0].adapterBalances[0].balances[0].base);
         assert.deepEqual(result[0].adapterBalances[0].balances[0].base.metadata, yfi);
         assert.equal(result[0].adapterBalances[0].balances[0].underlying.length, 0);
-        displayToken(result[0].adapterBalances[0].balances[1].base);
-        assert.deepEqual(result[0].adapterBalances[0].balances[1].base.metadata, yCrv);
-        assert.equal(result[0].adapterBalances[0].balances[1].underlying.length, 0);
-        displayToken(result[0].adapterBalances[0].balances[2].base);
-        assert.deepEqual(result[0].adapterBalances[0].balances[2].base.metadata, balancerDai98Yfi2);
-        assert.equal(result[0].adapterBalances[0].balances[2].underlying.length, 0);
-        displayToken(result[0].adapterBalances[0].balances[3].base);
-        assert.deepEqual(result[0].adapterBalances[0].balances[3].base.metadata, balancerYfi2yCrv98);
-        assert.equal(result[0].adapterBalances[0].balances[3].underlying.length, 0);
       });
   });
 });
