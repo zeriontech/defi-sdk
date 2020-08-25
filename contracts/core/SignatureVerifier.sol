f6ff7427bf02879a2864c1e150e963a2a7c0b9ed
Igor Sobolev <sobolev.igor@me.com>
2020-08-25 20:08:08 +0700

    2 audit fixes: improved sig verification

--- contracts/core/SignatureVerifier.sol
+++ contracts/core/SignatureVerifier.sol
@@ -126,19 +126,6 @@ contract SignatureVerifier {
         view
         returns (address payable)
     {
-        require(signature.length == 65, "SV: bad sig length!");
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        // solium-disable-next-line no-inline-assembly
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            r := mload(add(signature, 0x20))
-            s := mload(add(signature, 0x40))
-            v := byte(0, mload(add(signature, 0x60)))
-        }
-
         address signer = ecrecover(
             keccak256(
                 abi.encodePacked(
@@ -148,11 +135,10 @@ contract SignatureVerifier {
                     hash(data)
                 )
             ),
-            v,
-            r,
-            s
+            splitSignature(signature)
         );
 
+        require(signer != address(0), "SV: bad signature!");
         require(nonce_[signer] == data.nonce, "SV: bad nonce!");
 
         return payable(signer);
@@ -270,4 +256,44 @@ contract SignatureVerifier {
 
         return keccak256(outputsData);
     }
+
+    function splitSignature(
+        bytes memory signature
+    )
+        internal
+        pure
+        returns (uint8 v, bytes32 r, bytes32 s)
+    {
+        require(signature.length == 65, "SV: bad signature!");
+
+        assembly {
+            // first 32 bytes, after the length prefix.
+            r := mload(add(sig, 32))
+            // second 32 bytes.
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes).
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        // Reference: github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            revert("SV: bad 's'!");
+        }
+
+        if (v != 27 && v != 28) {
+            revert("SV: bad 'v'!");
+        }
+
+        return (v, r, s);
+    }
+
 }
