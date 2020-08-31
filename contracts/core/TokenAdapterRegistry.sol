7d7b5456effce4e7593edd7f9859cdba4a840e6e
Igor Sobolev <sobolev.igor@me.com>
2020-08-31 16:01:09 +0700

    added final components calculation

--- contracts/core/TokenAdapterRegistry.sol
+++ contracts/core/TokenAdapterRegistry.sol
@@ -45,9 +45,9 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
     address internal constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     /**
-     * @dev Fills in FullAbsoluteTokenAmount struct for an array of AbsoluteTokenAmount structs.
+     * @notice Fills in FullAbsoluteTokenAmount structs with underlying components.
      * @param absoluteTokenAmounts Array of AbsoluteTokenAmount structs consisting of
-     * token address and amount.
+     * token addresses and amounts.
      * @return Full absolute token amounts by token addresses and absolute amounts.
      */
     function getFullAbsoluteTokenAmounts(
@@ -63,16 +63,45 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
             new FullAbsoluteTokenAmount[](length);
 
         for (uint256 i = 0; i < length; i++) {
-            fullAbsoluteTokenAmounts[i] = getFullAbsoluteTokenAmount(absoluteTokenAmounts[i]);
+            fullAbsoluteTokenAmounts[i] = getFullAbsoluteTokenAmount(
+                absoluteTokenAmounts[i]
+            );
         }
 
         return fullAbsoluteTokenAmounts;
     }
 
     /**
-     * @dev Fills in FullAbsoluteTokenAmount struct for an array of tokens,
-     * amount is considered to be 10 ** decimals.
-     * @param tokens Array of tokens' addresses.
+     * @notice Fills in FullAbsoluteTokenAmount structs with final underlying components.
+     * @param absoluteTokenAmounts Array of AbsoluteTokenAmount structs consisting of
+     * token addresses and amounts.
+     * @return Final full absolute token amounts by token addresses and absolute amounts.
+     */
+    function getFinalFullAbsoluteTokenAmounts(
+        AbsoluteTokenAmount[] calldata absoluteTokenAmounts
+    )
+        external
+        view
+        returns (FullAbsoluteTokenAmount[] memory)
+    {
+        uint256 length = absoluteTokenAmounts.length;
+
+        FullAbsoluteTokenAmount[] memory finalFullAbsoluteTokenAmounts =
+            new FullAbsoluteTokenAmount[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            finalFullAbsoluteTokenAmounts[i] = getFinalFullAbsoluteTokenAmount(
+                absoluteTokenAmounts[i]
+            );
+        }
+
+        return finalFullAbsoluteTokenAmounts;
+    }
+
+    /**
+     * @notice Fills in FullAbsoluteTokenAmount structs with underlying components.
+     * @dev Amount is considered to be 10 ** decimals.
+     * @param tokens Array of token addresses.
      * @return Full absolute token amounts by token addresses.
      */
     function getFullAbsoluteTokenAmounts(
@@ -103,8 +132,41 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
     }
 
     /**
-     * @dev Fills in FullAbsoluteTokenAmount struct for a single token.
-     * @param absoluteTokenAmount Struct consisting of
+     * @notice Fills in FullAbsoluteTokenAmount structs with final underlying components.
+     * @dev Amount is considered to be 10 ** decimals.
+     * @param tokens Array of token addresses.
+     * @return Final full absolute token amounts by token addresses.
+     */
+    function getFinalFullAbsoluteTokenAmounts(
+        address[] calldata tokens
+    )
+        external
+        view
+        returns (FullAbsoluteTokenAmount[] memory)
+    {
+        uint256 length = tokens.length;
+
+        FullAbsoluteTokenAmount[] memory finalFullAbsoluteTokenAmounts =
+            new FullAbsoluteTokenAmount[](length);
+
+        uint8 decimals;
+        for (uint256 i = 0; i < length; i++) {
+            decimals = tokens[i] == ETH ? 18 : ERC20(tokens[i]).decimals();
+
+            finalFullAbsoluteTokenAmounts[i] = getFinalFullAbsoluteTokenAmount(
+                AbsoluteTokenAmount({
+                    token: tokens[i],
+                    amount: uint256(10) ** decimals
+                })
+            );
+        }
+
+        return finalFullAbsoluteTokenAmounts;
+    }
+
+    /**
+     * @notice Fills in FullAbsoluteTokenAmount struct with underlying components.
+     * @param absoluteTokenAmount AbsoluteTokenAmount struct consisting of
      * token address and its absolute amount.
      * @return Full absolute token amount by token address and absolute amount.
      */
@@ -116,6 +178,44 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
         returns (FullAbsoluteTokenAmount memory)
     {
         Component[] memory components = getComponents(absoluteTokenAmount);
+
+        return getFullAbsoluteTokenAmount(absoluteTokenAmount, components);
+    }
+
+    /**
+     * @notice Fills in FullAbsoluteTokenAmount struct with final underlying components.
+     * @param absoluteTokenAmount AbsoluteTokenAmount struct consisting of
+     * token address and its absolute amount.
+     * @return Final full absolute token amount by token address and absolute amount.
+     */
+    function getFinalFullAbsoluteTokenAmount(
+        AbsoluteTokenAmount memory absoluteTokenAmount
+    )
+        internal
+        view
+        returns (FullAbsoluteTokenAmount memory)
+    {
+        Component[] memory components = getFinalComponents(absoluteTokenAmount);
+
+        return getFullAbsoluteTokenAmount(absoluteTokenAmount, components);
+    }
+
+    /**
+     * @notice Fills in FullAbsoluteTokenAmount struct with underlying components.
+     * @param absoluteTokenAmount AbsoluteTokenAmount struct consisting of
+     * token address and its absolute amount.
+     * @param components Component struct consisting of
+     * token address and its absolute amount for each component.
+     * @return Full absolute token amount by token address, absolute amount, and components.
+     */
+    function getFullAbsoluteTokenAmount(
+        AbsoluteTokenAmount memory absoluteTokenAmount,
+        Component[] memory components
+    )
+        internal
+        view
+        returns (FullAbsoluteTokenAmount memory)
+    {
         uint256 length = components.length;
         AbsoluteTokenAmountMeta[] memory componentAbsoluteTokenAmounts =
             new AbsoluteTokenAmountMeta[](length);
@@ -124,7 +224,7 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
             componentAbsoluteTokenAmounts[i] = getAbsoluteTokenAmountMeta(
                 AbsoluteTokenAmount({
                     token: components[i].token,
-                    amount: components[i].rate
+                    amount: components[i].rate * absoluteTokenAmount.amount / 1e18
                 })
             );
         }
@@ -133,6 +233,79 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
             base: getAbsoluteTokenAmountMeta(absoluteTokenAmount),
             underlying: componentAbsoluteTokenAmounts
         });
+    }
+
+    /**
+     * @param absoluteTokenAmount AbsoluteTokenAmount struct consisting of
+     * token address and absolute amount.
+     * @return Final components by absolute token amount.
+     */
+    function getFinalComponents(
+        AbsoluteTokenAmount memory absoluteTokenAmount
+    )
+        internal
+        view
+        returns (Component[] memory)
+    {
+        uint256 totalLength = getFinalComponentsNumber(absoluteTokenAmount, true);
+        Component[] memory finalTokens = new Component[](totalLength);
+        uint256 length;
+        uint256 init = 0;
+
+        Component[] memory components = getComponents(absoluteTokenAmount);
+        Component[] memory finalComponents;
+
+        for (uint256 i = 0; i < components.length; i++) {
+            finalComponents = getFinalComponents(
+                AbsoluteTokenAmount({
+                    token: components[i].token,
+                    amount: components[i].rate * absoluteTokenAmount.amount / 1e18
+                })
+            );
+
+            length = finalComponents.length;
+
+            if (length == 0) {
+                finalTokens[init] = components[i];
+                init = init + 1;
+            } else {
+                for (uint256 j = 0; j < length; j++) {
+                    finalTokens[init + j] = finalComponents[j];
+                }
+
+                init = init + length;
+            }
+        }
+
+        return finalTokens;
+    }
+
+    /**
+     * @param absoluteTokenAmount AbsoluteTokenAmount struct consisting of
+     * token address and absolute amount.
+     * @param initial Whether the function call is initial or recursive.
+     * @return Final tokens number by absolute token amount.
+     */
+    function getFinalComponentsNumber(
+        AbsoluteTokenAmount memory absoluteTokenAmount,
+        bool initial
+    )
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 totalLength = 0;
+        Component[] memory components = getComponents(absoluteTokenAmount);
+
+        if (components.length == 0) {
+            return initial ? uint256(0) : uint256(1);
+        }
+
+        for (uint256 i = 0; i < components.length; i++) {
+            totalLength = totalLength + getFinalComponentsNumber(absoluteTokenAmount, false);
+        }
+
+        return totalLength;
     }
 
     /**
@@ -162,11 +335,6 @@ contract TokenAdapterRegistry is Ownable, TokenAdapterManager, TokenAdapterNames
             } catch {
                 components = new Component[](0);
             }
-        }
-
-        uint256 length = components.length;
-        for (uint256 i = 0; i < length; i++) {
-            components[i].rate = components[i].rate * absoluteTokenAmount.amount / 1e18;
         }
 
         return components;
