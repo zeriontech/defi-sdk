eecfb40510d003ab25e354ab40b4814929f89b9f
Roman Iftodi <roman.iftodi.me@gmail.com>
2020-09-08 22:55:25 +0300

    adding bZx to zerion sdk

---
+++ contracts/adapters/bzx/BzxTokenAdapter.sol
@@ -0,0 +1,70 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.5;
+pragma experimental ABIEncoderV2;
+
+import { ERC20 } from "../../ERC20.sol";
+import { TokenMetadata, Component } from "../../Structs.sol";
+import { TokenAdapter } from "../TokenAdapter.sol";
+
+
+interface TheProtocol{
+    function loanPoolToUnderlying(address iToken)
+        external
+        view
+        returns(address);
+}
+
+/**
+ * @title Token adapter for iTokens.
+ * @dev Implementation of TokenAdapter interface.
+ * @author Roman Iftodi <romeo8881@gmail.com>
+ */
+contract BzxTokenAdapter is TokenAdapter {
+    
+    TheProtocol internal constant bZxContract = TheProtocol(0xD8Ee69652E4e4838f2531732a46d1f7F584F0b7f);
+
+    /**
+     * @return TokenMetadata struct with ERC20-style token info.
+     * @dev Implementation of TokenAdapter interface function.
+     */
+    function getMetadata(address token) external view override returns (TokenMetadata memory) {
+        return TokenMetadata({
+            token: token,
+            name: ERC20(token).name(),
+            symbol: ERC20(token).symbol(),
+            decimals: ERC20(token).decimals()
+        });
+    }
+
+    /**
+     * @return Array of Component structs with underlying tokens rates for the given token.
+     * @dev Implementation of TokenAdapter interface function.
+     */
+    function getComponents(address token) external view override returns (Component[] memory) {
+        address underlying = bZxContract.loanPoolToUnderlying(token);
+
+        Component[] memory underlyingTokens = new Component[](1);
+
+        underlyingTokens[0] = Component({
+            token: underlying,
+            tokenType: "ERC20",
+            rate: uint256(1e18)
+        });
+
+        return underlyingTokens;
+    }
+}
---
+++ test/BzxDebtAdapter.js
@@ -0,0 +1,74 @@
+import displayToken from './helpers/displayToken';
+
+const AdapterRegistry = artifacts.require('AdapterRegistry');
+const ProtocolAdapter = artifacts.require('BzxDebtAdapter');
+const ERC20TokenAdapter = artifacts.require('ERC20TokenAdapter');
+
+contract('BzxDebtAdapter', () => {
+  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+  const testAddress = '0x7a22E7a4d912231f9c0FB6760c59Ab560188360b';
+
+  let accounts;
+  let adapterRegistry;
+  let protocolAdapterAddress;
+  let erc20TokenAdapterAddress;
+  const dai = [
+    daiAddress,
+    'Dai Stablecoin',
+    'DAI',
+    '18',
+  ];
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    await ProtocolAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        protocolAdapterAddress = result.address;
+      });
+    await ERC20TokenAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        erc20TokenAdapterAddress = result.address;
+      });
+    await AdapterRegistry.new({ from: accounts[0] })
+      .then((result) => {
+        adapterRegistry = result.contract;
+      });
+    await adapterRegistry.methods.addProtocols(
+      ['bZx'],
+      [[
+        'Mock Protocol Name',
+        'Mock protocol description',
+        'Mock website',
+        'Mock icon',
+        '0',
+      ]],
+      [[
+        protocolAdapterAddress,
+      ]],
+      [[[
+        daiAddress,
+      ]]],
+    )
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+    await adapterRegistry.methods.addTokenAdapters(
+      ['ERC20'],
+      [erc20TokenAdapterAddress],
+    )
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+  });
+
+  it('should return correct balances', async () => {
+    await adapterRegistry.methods['getBalances(address)'](testAddress)
+      .call()
+      .then((result) => {
+        displayToken(result[0].adapterBalances[0].balances[0].base);
+        assert.deepEqual(result[0].adapterBalances[0].balances[0].base.metadata, dai);
+      });
+  });
+});
