293ba8dff397b48d48f92554806355c27622a494
Igor Sobolev <sobolev.igor@me.com>
2020-09-11 17:13:53 +0700

    removed redundant code

--- contracts/adapters/sushi/SushiStakingAdapter.sol
+++ contracts/adapters/sushi/SushiStakingAdapter.sol
@@ -99,25 +99,4 @@ contract SushiStakingAdapter is ProtocolAdapter {
             return 0;
         }
     }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a, "SafeMath: subtraction overflow");
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-    }
 }
