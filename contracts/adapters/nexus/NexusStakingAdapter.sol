c15b1b29ff3bb6f4f1a13f0db2421bc97d13fe84
Igor Sobolev <sobolev.igor@me.com>
2020-09-15 08:22:08 +0700

    updated nexus adapter

--- contracts/adapters/nexus/NexusStakingAdapter.sol
+++ contracts/adapters/nexus/NexusStakingAdapter.sol
@@ -43,12 +43,15 @@ contract NexusStakingAdapter is ProtocolAdapter {
     string public constant override tokenType = "ERC20";
 
     address internal constant TOKEN_CONTROLLER = 0x5407381b6c251cFd498ccD4A1d877739CB7960B8;
+    address internal constant NXM = 0xd7c49CEE7E9188cCa6AD8FF264C1DA2e69D4Cf3B;
 
     /**
      * @return Amount of staked tokens + rewards by the given account.
      * @dev Implementation of ProtocolAdapter interface function.
      */
     function getBalance(address, address account) external view override returns (uint256) {
-        return TokenController(TOKEN_CONTROLLER).totalBalanceOf(account);
+        uint256 totalBalance = TokenController(TOKEN_CONTROLLER).totalBalanceOf(account);
+        uint256 tokenBalance = ERC20(NXM).balanceOf(account);
+        return totalBalance - tokenBalance;
     }
 }
