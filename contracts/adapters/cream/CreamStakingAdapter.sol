9d218c0a6163be84d50682165e3bdbdd8bda0991
Igor Sobolev <sobolev.igor@me.com>
2020-09-30 17:56:56 +0300

    updated cream staking adapter

--- contracts/adapters/cream/CreamStakingAdapter.sol
+++ contracts/adapters/cream/CreamStakingAdapter.sol
@@ -51,6 +51,10 @@ contract CreamStakingAdapter is ProtocolAdapter {
     address internal constant CREAM_CRYETH_WETH = 0x6a3B875854f5518E85Ef97620c5e7de75bbc3fA0;
     address internal constant CREAM_YYCRV_USDC = 0x661b94d96ADb18646e791A06576F7905a8d1BEF6;
     address internal constant CREAM_YFI_USDC = 0x7350c6D00D63AB5988250aea347f277c19BEA785;
+    address internal constant CREAM_UNI_WETH = 0x02566303A0E860EC66D3B79168459978B1b00C8e;
+    address internal constant CREAM_UNI_CREAM = 0x408e75C26e6182476940ece5b0Ba6491b4F13359;
+    address internal constant CREAM_CYUSD_CREAM = 0xaAF841fd6409c136FA4b960e22A92B45B26c9b41;
+    address internal constant CYUSD = 0x1D09144F3479bb805CB7c92346987420BcbDC10C;
     address internal constant CRCREAM = 0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0;
     address internal constant CREAM = 0x2ba592F78dB6436527729929AAf6c908497cB200;
 
@@ -64,7 +68,15 @@ contract CreamStakingAdapter is ProtocolAdapter {
     address internal constant CREAM_CRYETH_WETH_POOL = 0xCF679b2E16498a866Bd4CBda60d42f208084c6E1;
     address internal constant CREAM_YYCRV_USDC_POOL = 0xB8c3a282De181889EF20488e73e7A149a8C1bFe1;
     address internal constant CREAM_YFI_USDC_POOL = 0x2aB765c2B4A4E197fBAE769f86870F2310A04D61;
-    address internal constant CRCREAM_POOL = 0x71A808Fd21171d992ebc17678e8ae139079922d0;
+    address internal constant CREAM_UNI_WETH_POOL = 0xe539416a21F6D7eF7e143f1435484d333c274480;
+    address internal constant CREAM_UNI_CREAM_POOL = 0x1E5fe7bAd3672D0d0cc041B7154331EE461C3349;
+    address internal constant CREAM_CYUSD_CREAM_POOL = 0xAE76E0D0C4767E94Eb2898f42A96696Ae0061cEA;
+    address internal constant CREAM_1_YEAR_POOL = 0x780F75ad0B02afeb6039672E6a6CEDe7447a8b45;
+    address internal constant CREAM_2_YEAR_POOL = 0xBdc3372161dfd0361161e06083eE5D52a9cE7595;
+    address internal constant CREAM_3_YEAR_POOL = 0xD5586C1804D2e1795f3FBbAfB1FBB9099ee20A6c;
+    address internal constant CREAM_4_YEAR_POOL = 0xE618C25f580684770f2578FAca31fb7aCB2F5945;
+    address internal constant CYUSD_POOL = 0x0b471a71c3f6d4aADc7eb20AaB7D730Ae385e150;
+    address internal constant CRCREAM_POOL = 0x3ba3C0e8a9E5F4a01ce8e086B3d8e8a603a2129E;
 
     /**
      * @return Amount of staked tokens / rewards earned after staking for a given account.
@@ -73,6 +85,11 @@ contract CreamStakingAdapter is ProtocolAdapter {
     function getBalance(address token, address account) external view override returns (uint256) {
         if (token == CREAM) {
             uint256 totalRewards = 0;
+
+            totalRewards += ERC20(CREAM_1_YEAR_POOL).balanceOf(account);
+            totalRewards += ERC20(CREAM_2_YEAR_POOL).balanceOf(account);
+            totalRewards += ERC20(CREAM_3_YEAR_POOL).balanceOf(account);
+            totalRewards += ERC20(CREAM_4_YEAR_POOL).balanceOf(account);
 
             totalRewards += StakingRewards(BALANCER_CREAM_WETH_POOL).earned(account);
             totalRewards += StakingRewards(BALANCER_CREAM_WETH_POOL_2).earned(account);
@@ -84,6 +101,14 @@ contract CreamStakingAdapter is ProtocolAdapter {
             totalRewards += StakingRewards(CREAM_CRYETH_WETH_POOL).earned(account);
             totalRewards += StakingRewards(CREAM_YYCRV_USDC_POOL).earned(account);
             totalRewards += StakingRewards(CREAM_YFI_USDC_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_UNI_WETH_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_UNI_CREAM_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_CYUSD_CREAM_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_1_YEAR_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_2_YEAR_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_3_YEAR_POOL).earned(account);
+            totalRewards += StakingRewards(CREAM_4_YEAR_POOL).earned(account);
+            totalRewards += StakingRewards(CYUSD_POOL).earned(account);
             totalRewards += StakingRewards(CRCREAM_POOL).earned(account);
 
             return totalRewards;
@@ -112,6 +137,14 @@ contract CreamStakingAdapter is ProtocolAdapter {
             return ERC20(CREAM_YFI_USDC_POOL).balanceOf(account);
         } else if (token == CRCREAM) {
             return ERC20(CRCREAM_POOL).balanceOf(account);
+        } else if (token == CREAM_UNI_WETH) {
+            return ERC20(CREAM_UNI_WETH_POOL).balanceOf(account);
+        } else if (token == CREAM_UNI_CREAM) {
+            return ERC20(CREAM_UNI_CREAM_POOL).balanceOf(account);
+        } else if (token == CREAM_CYUSD_CREAM) {
+            return ERC20(CREAM_CYUSD_CREAM_POOL).balanceOf(account);
+        } else if (token == CYUSD) {
+            return ERC20(CYUSD_POOL).balanceOf(account);
         } else {
             return 0;
         }
