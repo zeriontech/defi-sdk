c1eae5d91d0937ab0c01be7964936b91579e55e4
Igor Sobolev <sobolev.igor@me.com>
2020-10-21 18:57:54 +0300

    curve + harvest updates

--- contracts/adapters/harvest/HarvestStakingAdapter.sol
+++ contracts/adapters/harvest/HarvestStakingAdapter.sol
@@ -43,39 +43,37 @@ contract HarvestStakingAdapter is ProtocolAdapter {
     string public constant override tokenType = "ERC20";
 
     address internal constant FARM = 0xa0246c9032bC3A600820415aE600c6388619A14D;
-    address internal constant FDAI = 0xe85C8581e60D7Cd32Bbfd86303d2A4FA6a951Dac;
-    address internal constant FUSDC = 0xc3F7ffb5d5869B3ade9448D094d81B0521e8326f;
-    address internal constant FUSDT = 0xc7EE21406BB581e741FBb8B21f213188433D9f2F;
-    address internal constant BALANCER_USDC_95_FARM_5 = 0x0395e4A17fF11D36DaC9959f2D7c8Eca10Fe89c9;
-    address internal constant UNISWAP_V2_USDC_FARM = 0x514906FC121c7878424a5C928cad1852CC545892;
-    address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal constant UNI_FARM_USDC = 0xF553E1f826f42716cDFe02bde5ee76b2a52fc7EB;
+    address internal constant F_WBTC = 0x5d9d25c7C457dD82fc8668FFC6B9746b674d4EcB;
+    address internal constant F_WETH = 0xFE09e53A81Fe2808bc493ea64319109B5bAa573e;
+    address internal constant F_DAI = 0xab7FA2B2985BCcfC13c6D86b1D5A17486ab1e04C;
+    address internal constant F_USDC = 0xf0358e8c3CD5Fa238a29301d0bEa3D63A17bEdBE;
+    address internal constant F_USDT = 0x053c80eA73Dc6941F518a68E2FC52Ac45BDE7c9C;
+    address internal constant F_RENBTC = 0xC391d1b08c1403313B0c28D47202DFDA015633C4;
+    address internal constant F_CRV_RENWBTC = 0x9aA8F427A17d6B0d91B6262989EdC7D45d6aEdf8;
+    address internal constant F_UNI_WETH_USDT = 0x7DDc3ffF0612E75Ea5ddC0d6Bd4e268f70362Cff;
+    address internal constant F_UNI_WETH_USDC = 0xA79a083FDD87F73c2f983c5551EC974685D6bb36;
+    address internal constant F_UNI_WETH_DAI = 0x307E2752e8b8a9C29005001Be66B1c012CA9CDB7;
+    address internal constant F_UNI_WETH_WBTC = 0x01112a60f427205dcA6E229425306923c3Cc2073;
+    address internal constant F_TUSD = 0x7674622c63Bee7F46E86a4A5A18976693D54441b;
+    address internal constant F_SUSHI_WBTC_TBTC = 0xF553E1f826f42716cDFe02bde5ee76b2a52fc7EB;
 
-    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address internal constant LINK = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
-    address internal constant YFI = 0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e;
-    address internal constant SUSHI = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2;
-    address internal constant YFV = 0x45f24BaEef268BB6d63AEe5129015d69702BCDfa;
-    address internal constant YFII = 0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83;
-    address internal constant OGN = 0x8207c1FfC5B6804F6024322CcF34F29c3541Ae26;
-    address internal constant UNISWAP_V2_BASED_SUSD = 0xaAD22f5543FCDaA694B68f94Be177B561836AE57;
-    address internal constant UNISWAP_V2_PASTA_WETH = 0xE92346d9369Fe03b735Ed9bDeB6bdC2591b8227E;
+    address internal constant FARM_POOL = 0x8f5adC58b32D4e5Ca02EAC0E293D35855999436C;
+    address internal constant UNI_FARM_USDC_POOL = 0x99b0d6641A63Ce173E6EB063b3d3AED9A35Cf9bf;
+    address internal constant F_WBTC_POOL = 0x917d6480Ec60cBddd6CbD0C8EA317Bcc709EA77B;
+    address internal constant F_WETH_POOL = 0x3DA9D911301f8144bdF5c3c67886e5373DCdff8e;
+    address internal constant F_DAI_POOL = 0x15d3A64B2d5ab9E152F16593Cdebc4bB165B5B4A;
+    address internal constant F_USDC_POOL = 0x4F7c28cCb0F1Dbd1388209C67eEc234273C878Bd;
+    address internal constant F_USDT_POOL = 0x6ac4a7AB91E6fD098E13B7d347c6d4d1494994a2;
+    address internal constant F_RENBTC_POOL = 0x7b8Ff8884590f44e10Ea8105730fe637Ce0cb4F6;
+    address internal constant F_CRV_RENWBTC_POOL = 0xA3Cf8D1CEe996253FAD1F8e3d68BDCba7B3A3Db5;
+    address internal constant F_UNI_WETH_USDT_POOL = 0x75071F2653fBC902EBaff908d4c68712a5d1C960;
+    address internal constant F_UNI_WETH_USDC_POOL = 0x156733b89Ac5C704F3217FEe2949A9D4A73764b5;
+    address internal constant F_UNI_WETH_DAI_POOL = 0x7aeb36e22e60397098C2a5C51f0A5fB06e7b859c;
+    address internal constant F_UNI_WETH_WBTC_POOL = 0xF1181A71CC331958AE2cA2aAD0784Acfc436CB93;
+    address internal constant F_TUSD_POOL = 0xeC56a21CF0D7FeB93C25587C12bFfe094aa0eCdA;
+    address internal constant F_SUSHI_WBTC_TBTC_POOL = 0x9523FdC055F503F73FF40D7F66850F409D80EF34;
 
-    address internal constant FDAI_POOL = 0xF9E5f9024c2f3f2908A1d0e7272861a767C9484b;
-    address internal constant FUSDC_POOL = 0xE1f9A3EE001a2EcC906E8de637DBf20BB2d44633;
-    address internal constant FUSDT_POOL = 0x5bd997039FFF16F653EF15D1428F2C791519f58d;
-    address internal constant BALANCER_POOL = 0x6f8A975758436A5Ec38d2f9d2336504430465517;
-    address internal constant UNISWAP_POOL = 0x99b0d6641A63Ce173E6EB063b3d3AED9A35Cf9bf;
-    address internal constant PROFIT_SHARING_DAI_POOL = 0xae024F29C26D6f71Ec71658B1980189956B0546D;
-
-    address internal constant WETH_POOL = 0xE604Fd5b1317BABd0cF2c72F7F5f2AD8c00Adbe1;
-    address internal constant LINK_POOL = 0xa112c2354d27c2Fb3370cc5d027B28987117a268;
-    address internal constant YFI_POOL = 0x84646F736795a8bC22Ab34E05c8982CD058328C7;
-    address internal constant SUSHI_POOL = 0x4938960C507A4d7094C53A8cDdCF925835393B8f;
-    address internal constant YFV_POOL = 0x3631A32c959C5c52BC90AB5b7D212a8D00321918;
-    address internal constant YFII_POOL = 0xC97DDAa8091aBaF79A4910b094830CCE5cDd78f4;
-    address internal constant OGN_POOL = 0xF71042C88458ff1702c3870f62F4c764712Cc9F0;
-    address internal constant BASED_POOL = 0xb3b56c7BDc87F9DeB7972cD8b5c09329ce421F89;
-    address internal constant PASTA_POOL = 0xC6f39CFf6797baC5e29275177b6E8e315cF87D95;
 
     /**
      * @return Amount of staked tokens / rewards earned after staking for a given account.
@@ -85,53 +83,52 @@ contract HarvestStakingAdapter is ProtocolAdapter {
         if (token == FARM) {
             uint256 totalRewards = 0;
 
-            totalRewards += ERC20(PROFIT_SHARING_DAI_POOL).balanceOf(account);
-            totalRewards += StakingRewards(FDAI_POOL).earned(account);
-            totalRewards += StakingRewards(FUSDC_POOL).earned(account);
-            totalRewards += StakingRewards(FUSDT_POOL).earned(account);
-            totalRewards += StakingRewards(BALANCER_POOL).earned(account);
-            totalRewards += StakingRewards(UNISWAP_POOL).earned(account);
-            totalRewards += StakingRewards(WETH_POOL).earned(account);
-            totalRewards += StakingRewards(LINK_POOL).earned(account);
-            totalRewards += StakingRewards(YFI_POOL).earned(account);
-            totalRewards += StakingRewards(SUSHI_POOL).earned(account);
-            totalRewards += StakingRewards(YFV_POOL).earned(account);
-            totalRewards += StakingRewards(YFII_POOL).earned(account);
-            totalRewards += StakingRewards(OGN_POOL).earned(account);
-            totalRewards += StakingRewards(BASED_POOL).earned(account);
-            totalRewards += StakingRewards(PASTA_POOL).earned(account);
+            totalRewards += ERC20(FARM_POOL).balanceOf(account);
+            totalRewards += StakingRewards(FARM_POOL).earned(account);
+            totalRewards += StakingRewards(UNI_FARM_USDC_POOL).earned(account);
+            totalRewards += StakingRewards(F_WBTC_POOL).earned(account);
+            totalRewards += StakingRewards(F_WETH_POOL).earned(account);
+            totalRewards += StakingRewards(F_DAI_POOL).earned(account);
+            totalRewards += StakingRewards(F_USDC_POOL).earned(account);
+            totalRewards += StakingRewards(F_USDT_POOL).earned(account);
+            totalRewards += StakingRewards(F_RENBTC_POOL).earned(account);
+            totalRewards += StakingRewards(F_CRV_RENWBTC_POOL).earned(account);
+            totalRewards += StakingRewards(F_UNI_WETH_USDT_POOL).earned(account);
+            totalRewards += StakingRewards(F_UNI_WETH_USDC_POOL).earned(account);
+            totalRewards += StakingRewards(F_UNI_WETH_DAI_POOL).earned(account);
+            totalRewards += StakingRewards(F_UNI_WETH_WBTC_POOL).earned(account);
+            totalRewards += StakingRewards(F_TUSD_POOL).earned(account);
+            totalRewards += StakingRewards(F_SUSHI_WBTC_TBTC_POOL).earned(account);
 
             return totalRewards;
-        } else if (token == DAI) {
-            return StakingRewards(PROFIT_SHARING_DAI_POOL).earned(account);
-        } else if (token == FDAI) {
-            return ERC20(FDAI_POOL).balanceOf(account);
-        } else if (token == FUSDC) {
-            return ERC20(FUSDC_POOL).balanceOf(account);
-        } else if (token == FUSDT) {
-            return ERC20(FUSDT_POOL).balanceOf(account);
-        } else if (token == BALANCER_USDC_95_FARM_5) {
-            return ERC20(BALANCER_POOL).balanceOf(account);
-        } else if (token == UNISWAP_V2_USDC_FARM) {
-            return ERC20(UNISWAP_POOL).balanceOf(account);
-        } else if (token == WETH) {
-            return ERC20(WETH_POOL).balanceOf(account);
-        } else if (token == LINK) {
-            return ERC20(LINK_POOL).balanceOf(account);
-        } else if (token == YFI) {
-            return ERC20(YFI_POOL).balanceOf(account);
-        } else if (token == SUSHI) {
-            return ERC20(SUSHI_POOL).balanceOf(account);
-        } else if (token == YFV) {
-            return ERC20(YFV_POOL).balanceOf(account);
-        } else if (token == YFII) {
-            return ERC20(YFII_POOL).balanceOf(account);
-        } else if (token == OGN) {
-            return ERC20(OGN_POOL).balanceOf(account);
-        } else if (token == UNISWAP_V2_BASED_SUSD) {
-            return ERC20(BASED_POOL).balanceOf(account);
-        } else if (token == UNISWAP_V2_PASTA_WETH) {
-            return ERC20(PASTA_POOL).balanceOf(account);
+        } else if (token == UNI_FARM_USDC) {
+            return ERC20(UNI_FARM_USDC_POOL).balanceOf(account);
+        } else if (token == F_WBTC) {
+            return ERC20(F_WBTC_POOL).balanceOf(account);
+        } else if (token == F_WETH) {
+            return ERC20(F_WETH_POOL).balanceOf(account);
+        } else if (token == F_DAI) {
+            return ERC20(F_DAI_POOL).balanceOf(account);
+        } else if (token == F_USDC) {
+            return ERC20(F_USDC_POOL).balanceOf(account);
+        } else if (token == F_USDT) {
+            return ERC20(F_USDT_POOL).balanceOf(account);
+        } else if (token == F_RENBTC) {
+            return ERC20(F_RENBTC_POOL).balanceOf(account);
+        } else if (token == F_CRV_RENWBTC) {
+            return ERC20(F_CRV_RENWBTC_POOL).balanceOf(account);
+        } else if (token == F_UNI_WETH_USDT) {
+            return ERC20(F_UNI_WETH_USDT_POOL).balanceOf(account);
+        } else if (token == F_UNI_WETH_USDC) {
+            return ERC20(F_UNI_WETH_USDC_POOL).balanceOf(account);
+        } else if (token == F_UNI_WETH_DAI) {
+            return ERC20(F_UNI_WETH_DAI_POOL).balanceOf(account);
+        } else if (token == F_UNI_WETH_WBTC) {
+            return ERC20(F_UNI_WETH_WBTC_POOL).balanceOf(account);
+        } else if (token == F_TUSD) {
+            return ERC20(F_TUSD_POOL).balanceOf(account);
+        } else if (token == F_SUSHI_WBTC_TBTC) {
+            return ERC20(F_SUSHI_WBTC_TBTC_POOL).balanceOf(account);
         } else {
             return 0;
         }
