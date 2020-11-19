da25db1f70fac69ac18635df31e0faceea307033
Igor Sobolev <cr-igorsobolev@yandex.ru>
2020-11-19 18:38:38 +0300

    updated compound registry

--- contracts/adapters/compound/CompoundRegistry.sol
+++ contracts/adapters/compound/CompoundRegistry.sol
@@ -17,6 +17,8 @@
 
 pragma solidity 0.7.3;
 
+import { Ownable } from "../../core/Ownable.sol";
+
 /**
  * @title Registry for Compound contracts.
  * @dev Implements the only function - getCToken(address).
@@ -24,21 +26,23 @@ pragma solidity 0.7.3;
  * of CToken contract for the given token address.
  * @author Igor Sobolev <sobolev@zerion.io>
  */
-contract CompoundRegistry {
-    mapping(address => address) internal cTokens;
+contract CompoundRegistry is Ownable {
+    mapping(address => address) internal cToken_;
 
-    constructor() {
-        cTokens[0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359] = 0xF5DCe57282A584D2746FaF1593d3121Fcac444dC;
-        cTokens[0x1985365e9f78359a9B6AD760e32412f4a445E862] = 0x158079Ee67Fce2f58472A96584A73C7Ab9AC95c1;
-        cTokens[0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE] = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
-        cTokens[0x6B175474E89094C44Da98b954EedeAC495271d0F] = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
-        cTokens[0x0D8775F648430679A709E98d2b0Cb6250d2887EF] = 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E;
-        cTokens[0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599] = 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4;
-        cTokens[0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48] = 0x39AA39c021dfbaE8faC545936693aC917d5E7563;
-        cTokens[0xE41d2489571d322189246DaFA5ebDe1F4699F498] = 0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407;
+    function setCTokens(address[] calldata tokens, address[] calldata cTokens) external {
+        uint256 length = tokens.length;
+        require(cTokens.length == length, "CR: inconsistent arrays");
+
+        for (uint256 i = 0; i < length; i++) {
+            setCToken(tokens[i], cTokens[i]);
+        }
+    }
+
+    function setCToken(address token, address cToken) internal {
+        cToken_[token] = cToken;
     }
 
     function getCToken(address token) external view returns (address) {
-        return cTokens[token];
+        return cToken_[token];
     }
 }
