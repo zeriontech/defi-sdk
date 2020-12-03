89b787884b3815c743777b4b4857e42678c93911
Mick de Graaf <mick@mickdegraaf.nl>
2020-12-03 14:45:00 +0100

    ExperiPieAdapter

--- contracts/interfaces/ExperiPie.sol
+++ contracts/interfaces/ExperiPie.sol
@@ -3,4 +3,6 @@ pragma experimental ABIEncoderV2;
 
 interface ExperiPie {
     function balance(address token) external view returns (uint256);
+
+    function getTokens() external view returns (address[] memory);
 }
