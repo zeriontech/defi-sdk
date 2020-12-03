fd5852dd4820ab1be52f23ac045f2511dcdf72ac
Mick de Graaf <mick@mickdegraaf.nl>
2020-12-03 14:45:00 +0100

    ExperiPieAdapter

--- contracts/adapters/pieDAO/PieDAOExperiPieTokenAdapter.sol
+++ contracts/adapters/pieDAO/PieDAOExperiPieTokenAdapter.sol
@@ -18,11 +18,9 @@
 pragma solidity 0.7.3;
 pragma experimental ABIEncoderV2;
 
-import { ERC20 } from "../../interfaces/ERC20.sol";
+import { ERC20 } from "../../shared/ERC20.sol";
 import { Component } from "../../shared/Structs.sol";
 import { TokenAdapter } from "../TokenAdapter.sol";
-import { BPool } from "../../interfaces/BPool.sol";
-import { PBasicSmartPool } from "../../interfaces/PBasicSmartPool.sol";
 import { ExperiPie } from "../../interfaces/ExperiPie.sol";
 
 /**
@@ -30,24 +28,22 @@ import { ExperiPie } from "../../interfaces/ExperiPie.sol";
  * @dev Implementation of TokenAdapter abstract contract.
  * @author Mick de Graaf <mick@dexlab.io>
  */
-contract PieDAOPieTokenAdapter is TokenAdapter {
+contract PieDAOExperiPieTokenAdapter is TokenAdapter {
     /**
      * @return Array of Component structs with underlying tokens rates for the given asset.
      * @dev Implementation of TokenAdapter abstract contract function.
      */
     function getComponents(address token) external view override returns (Component[] memory) {
-        address[] memory tokens = PBasicSmartPool(token).getTokens();
+        address[] memory tokens = ExperiPie(token).getTokens();
+
         uint256 totalSupply = ERC20(token).totalSupply();
 
         Component[] memory components = new Component[](tokens.length);
 
-        address bPool = PBasicSmartPool(token).getBPool();
-
-        // if smart pool
         for (uint256 i = 0; i < tokens.length; i++) {
             components[i] = Component({
                 token: tokens[i],
-                rate: int256((BPool(bPool).getBalance(tokens[i]) * 1e18) / totalSupply)
+                rate: int256((ExperiPie(token).balance(tokens[i]) * 1e18) / totalSupply)
             });
         }
 
--- test/adapters/PieDAOExperiPieAdapter.js
+++ test/adapters/PieDAOExperiPieAdapter.js
@@ -1,4 +1,3 @@
-import displayToken from '../helpers/displayToken';
 import convertToBytes32 from '../helpers/convertToBytes32';
 
 const PIE_DAO_ASSET_ADAPTER = convertToBytes32('PeiDAO Pie Adapter');
@@ -7,14 +6,11 @@ const EMPTY_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000
 const ProtocolAdapterRegistry = artifacts.require('ProtocolAdapterRegistry');
 const TokenAdapterRegistry = artifacts.require('TokenAdapterRegistry');
 const ProtocolAdapter = artifacts.require('ERC20ProtocolAdapter');
-const TokenAdapter = artifacts.require('PieDAOPieTokenAdapter');
+const TokenAdapter = artifacts.require('PieDAOExperiPieTokenAdapter');
 const ERC20TokenAdapter = artifacts.require('ERC20TokenAdapter');
 
-contract('PieDAOPieAdapter', () => {
-  const BTCPPAddress = '0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd';
-  const USDPPAddress = '0x9A48BD0EC040ea4f1D3147C025cd4076A2e71e3e';
-  // Balancer BTC++ / USD++ Pool
-  const testAddress = '0x7d2F4bcB767eB190Aed0f10713fe4D9c07079ee8';
+contract('PieDAOExperiPieAdapter', () => {
+  const DXYAddress = '0x992e9f1d29e2fdb57a9e09a78e122fafe3720cc5';
 
   let accounts;
   let protocolAdapterRegistry;
@@ -23,24 +19,45 @@ contract('PieDAOPieAdapter', () => {
   let tokenAdapterAddress;
   let erc20TokenAdapterAddress;
 
-  const wbtc = [
-    'Wrapped BTC',
-    'WBTC',
-    '8',
-  ];
-  const pbtc = [
-    'pTokens BTC',
-    'pBTC',
+  const link = [
+    'ChainLink Token',
+    'LINK',
     '18',
   ];
-  const imbtc = [
-    'The Tokenized Bitcoin',
-    'imBTC',
-    '8',
+
+  const yfi = [
+    'yearn.finance',
+    'YFI',
+    '18',
   ];
-  const sbtc = [
-    'Synth sBTC',
-    'sBTC',
+
+  const aave = [
+    'Aave Token',
+    'AAVE',
+    '18',
+  ];
+
+  const uni = [
+    'Uniswap',
+    'UNI',
+    '18',
+  ];
+
+  const snx = [
+    'Synthetix Network Token',
+    'SNX',
+    '18',
+  ];
+
+  const comp = [
+    'Compound',
+    'COMP',
+    '18',
+  ];
+
+  const mkr = [
+    'Maker',
+    'MKR',
     '18',
   ];
 
@@ -74,8 +91,7 @@ contract('PieDAOPieAdapter', () => {
         protocolAdapterAddress,
       ],
       [[
-        BTCPPAddress,
-        USDPPAddress,
+        DXYAddress,
       ]],
     )
       .send({
@@ -91,14 +107,6 @@ contract('PieDAOPieAdapter', () => {
         gas: '1000000',
       });
     await tokenAdapterRegistry.methods.addTokenAdapterNamesByHashes(
-      [BTCPPAddress],
-      [convertToBytes32('PieDAO Pie Token')],
-    )
-      .send({
-        from: accounts[0],
-        gas: '1000000',
-      });
-    await tokenAdapterRegistry.methods.addTokenAdapterNamesByHashes(
       [DXYAddress],
       [convertToBytes32('PieDAO Pie Token')],
     )
@@ -108,27 +116,14 @@ contract('PieDAOPieAdapter', () => {
       });
   });
 
-  it('should return correct balances', async () => {
-    await protocolAdapterRegistry.methods['getBalances(address)'](testAddress)
-      .call()
-      .then(async (result) => {
-        await displayToken(tokenAdapterRegistry, result[0].tokenBalances[0]);
-        await displayToken(tokenAdapterRegistry, result[0].tokenBalances[1]);
-      });
-  });
-
-  it('should return correct underlying tokens', async () => {
-    await tokenAdapterRegistry.methods.getFullTokenBalances(
-      [
-        BTCPPAddress,
-      ],
-    )
-      .call()
-      .then((result) => {
-        assert.deepEqual(result[0].underlying[0].erc20metadata, wbtc);
-        assert.deepEqual(result[0].underlying[1].erc20metadata, pbtc);
-        assert.deepEqual(result[0].underlying[2].erc20metadata, imbtc);
-        assert.deepEqual(result[0].underlying[3].erc20metadata, sbtc);
-      });
+  it('should return correct underlying tokens for experiPie', async () => {
+    const result = await tokenAdapterRegistry.methods.getFullTokenBalances([DXYAddress]).call();
+    assert.deepEqual(result[0].underlying[0].erc20metadata, link);
+    assert.deepEqual(result[0].underlying[1].erc20metadata, yfi);
+    assert.deepEqual(result[0].underlying[2].erc20metadata, aave);
+    assert.deepEqual(result[0].underlying[3].erc20metadata, uni);
+    assert.deepEqual(result[0].underlying[4].erc20metadata, snx);
+    assert.deepEqual(result[0].underlying[5].erc20metadata, comp);
+    assert.deepEqual(result[0].underlying[6].erc20metadata, mkr);
   });
 });
