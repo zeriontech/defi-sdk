5b723298dded5e85b433a87fba1bece2818f78b9
Igor Sobolev <cr-igorsobolev@yandex.ru>
2020-12-17 19:06:34 +0300

    updated tests

---
+++ test/BancorLockedTokensAdapter.js
@@ -0,0 +1,25 @@
+const ProtocolAdapter = artifacts.require('BancorLiquidityProtectionAdapter');
+
+contract.only('BancorLockedTokensAdapter', () => {
+  const bntAddress = '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C';
+  const testAddress = '0x42b9dF65B219B3dD36FF330A4dD8f327A6Ada990';
+
+  let accounts;
+  let protocolAdapterContract;
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    await ProtocolAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        protocolAdapterContract = result.contract;
+      });
+  });
+
+  it('should return correct balances', async () => {
+    await protocolAdapterContract.methods['getBalance(address,address)'](bntAddress, testAddress)
+      .call()
+      .then((result) => {
+        console.log(result);
+      });
+  });
+});
