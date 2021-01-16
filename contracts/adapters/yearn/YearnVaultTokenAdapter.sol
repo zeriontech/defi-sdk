78040f6d795de876d6b66ed5eca029002345f8d9
Igor Sobolev <cr-igorsobolev@yandex.ru>
2021-01-16 22:55:24 +0300

    added yearn vaults adapters

---
+++ contracts/adapters/yearn/YearnVaultTokenAdapter.sol
@@ -0,0 +1,72 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+pragma solidity 0.7.6;
+pragma experimental ABIEncoderV2;
+
+import { ERC20 } from "../../interfaces/ERC20.sol";
+import { Component } from "../../shared/Structs.sol";
+import { TokenAdapter } from "../TokenAdapter.sol";
+
+/**
+ * @dev Vault contract interface.
+ * Only the functions required for YearnVaultTokenAdapter contract are added.
+ * The Vault contracts is available here
+ * https://github.com/iearn-finance/yearn-vaults/blob/master/contracts/Vault.vy.
+ */
+interface Vault {
+    function token() external view returns (address);
+
+    function pricePerShare() external view returns (uint256);
+}
+
+/**
+ * @title Token adapter for Yearn Token Vaults.
+ * @dev Implementation of TokenAdapter abstract contract.
+ * @author Igor Sobolev <sobolev@zerion.io>
+ */
+contract YearnVaultTokenAdapter is TokenAdapter {
+
+    /**
+     * @return TokenMetadata struct with ERC20-style token info.
+     * @dev Implementation of TokenAdapter interface function.
+     */
+    function getMetadata(address token) external view override returns (TokenMetadata memory) {
+        return TokenMetadata({
+        token: token,
+            name: ERC20(token).name(),
+            symbol: ERC20(token).symbol(),
+            decimals: ERC20(token).decimals()
+        });
+    }
+
+    /**
+     * @return Array of Component structs with underlying tokens rates for the given token.
+     * @dev Implementation of TokenAdapter abstract contract function.
+     */
+    function getComponents(address token) external view override returns (Component[] memory) {
+        Component[] memory components = new Component[](1);
+
+        components[0] = Component({
+            token: Vault(token).token(),
+            tokenType: "ERC20",
+            rate: Vault(token).pricePerShare()
+        });
+
+        return components;
+    }
+}
