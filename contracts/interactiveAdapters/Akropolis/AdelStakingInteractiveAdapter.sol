a7c7a007b37a4d3f04d47bf782ee10c34dcd82b1
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2021-01-21 12:03:58 +0300

    DD-1744 Fix comments

--- contracts/interactiveAdapters/Akropolis/AdelStakingInteractiveAdapter.sol
+++ contracts/interactiveAdapters/Akropolis/AdelStakingInteractiveAdapter.sol
@@ -55,10 +55,13 @@ contract AdelStakingInteractiveAdapter is InteractiveAdapter, ERC20ProtocolAdapt
 
     address internal constant STAKING = 0x1A547c3dd03c39Fb2b5aEaFC524033879bD28F13;
 
+    address internal constant ADEL = 0x94d863173EE77439E4292284fF13fAD54b3BA182;
+
     /**
      * @notice Stake ADEL token to the ADEL Staking
      * @param tokenAmounts Array with one element - TokenAmount struct with
      * ADEL token address, token amount to be stake, and amount type.
+     * @param data user address encoded in bytes data
      * @return tokensToBeWithdrawn empty array
      * @dev Implementation of InteractiveAdapter function.
      */
@@ -68,12 +71,14 @@ contract AdelStakingInteractiveAdapter is InteractiveAdapter, ERC20ProtocolAdapt
         override
         returns (address[] memory tokensToBeWithdrawn)
     {
-        require(tokenAmounts.length == 1, "ADELIA: should be 1 tokenAmount[1]");
+        require(
+            tokenAmounts.length == 1 && tokenAmounts[0].token == ADEL,
+            "ADELIA: should be 1 tokenAmount[1]"
+        );
 
-        address token = tokenAmounts[0].token;
         uint256 amount = getAbsoluteAmountDeposit(tokenAmounts[0]);
 
-        ERC20(token).safeApprove(STAKING, amount, "ADELIA");
+        ERC20(tokenAmounts[0].token).safeApprove(STAKING, amount, "ADELIA");
 
         //get user data from calldata
         address userAddress = abi.decode(data, (address));
@@ -89,24 +94,25 @@ contract AdelStakingInteractiveAdapter is InteractiveAdapter, ERC20ProtocolAdapt
     }
 
     /**
-     * @notice Withdraws tokens from the Adel Staking
-     * @param tokenAmounts Array with one element - TokenAmount struct with
-     *  ADEL address, ADEL amount to be unstake
+     * @notice Withdraws tokens from the Adel Staking. it always withdraws all the tokens
+     * @param tokenAmounts should be empty
+     * @param data token address encoded in bytes data
      * @return tokensToBeWithdrawn Array with one element - ADEL token.
      * @dev Implementation of InteractiveAdapter function.
      */
-    function withdraw(TokenAmount[] calldata tokenAmounts, bytes calldata)
+    function withdraw(TokenAmount[] calldata tokenAmounts, bytes calldata data)
         external
         payable
         override
         returns (address[] memory tokensToBeWithdrawn)
     {
-        require(tokenAmounts.length == 1, "ADELIA: should be 1 tokenAmount[2]");
-
-        address token = tokenAmounts[0].token;
+        require(
+            tokenAmounts.length == 0 && abi.decode(data, (address)) == ADEL,
+            "ADELIA: should be 0 tokenAmount"
+        );
 
         tokensToBeWithdrawn = new address[](1);
-        tokensToBeWithdrawn[0] = tokenAmounts[0].token;
+        tokensToBeWithdrawn[0] = abi.decode(data, (address));
 
         // solhint-disable-next-line no-empty-blocks
         try AdelStaking(STAKING).unstakeAllUnlocked("0x")  {} catch Error(string memory reason) {
