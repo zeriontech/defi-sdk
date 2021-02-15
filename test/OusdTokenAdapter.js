0c796be82a2e1e52144e34911830a40499ff4105
Domen Grabec <grabec@gmail.com>
2021-02-15 17:14:15 +0100

    initial commit of OUSD integration

---
+++ test/OusdTokenAdapter.js
@@ -0,0 +1,39 @@
+const TokenAdapter = artifacts.require('OusdTokenAdapter');
+
+contract.only('OusdTokenAdapter', () => {
+  const tokenAddress = '0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86';
+
+  let accounts;
+  let tokenAdapter;
+  const OUSD = [
+    tokenAddress,
+    'Origin Dollar',
+    'OUSD',
+    '18',
+  ];
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    await TokenAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        tokenAdapter = result.contract;
+      });
+  });
+
+  it('should return correct components', async () => {
+    await tokenAdapter.methods['getComponents(address)'](tokenAddress)
+      .call()
+      .then((result) => {
+        console.dir(result, { depth: null });
+      });
+  });
+
+  it('should return correct metadata', async () => {
+    await tokenAdapter.methods['getMetadata(address)'](tokenAddress)
+      .call()
+      .then((result) => {
+        assert.deepEqual(result, OUSD);
+      });
+  });
+});
