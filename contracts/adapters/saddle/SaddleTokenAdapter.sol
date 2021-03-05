67e59a90fca136595cabfe11fcde6dd8028a38cf
Igor Sobolev <cr-igorsobolev@yandex.ru>
2021-03-05 14:37:09 +0300

    added saddle adapter

---
+++ contracts/adapters/saddle/SaddleTokenAdapter.sol
@@ -0,0 +1,97 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.5;
+pragma experimental ABIEncoderV2;
+
+import { ERC20 } from "../../ERC20.sol";
+import { TokenMetadata, Component } from "../../Structs.sol";
+import { TokenAdapter } from "../TokenAdapter.sol";
+
+/**
+ * @dev CurveRegistry contract interface.
+ * Only the functions required for SaddleTokenAdapter contract are added.
+ * The CurveRegistry contract is available here
+ * github.com/zeriontech/defi-sdk/blob/master/contracts/adapters/curve/CurveRegistry.sol.
+ */
+interface CurveRegistry {
+    function getSwapAndTotalCoins(address) external view returns (address, uint256);
+    function getName(address) external view returns (string memory);
+}
+
+
+/**
+ * @dev Swap contract interface.
+ * Only the functions required for SaddleTokenAdapter contract are added.
+ * The Swap contract is available here
+ * github.com/saddle-finance/saddle-contract/blob/master/contracts/Swap.sol.
+ */
+interface Swap {
+    function getToken(uint8) external view returns (address);
+    function getTokenBalance(uint8) external view returns (uint256);
+    function getTokenIndex(address) external view returns (uint8);
+}
+
+
+/**
+ * @title Token adapter for Saddle pool tokens.
+ * @dev Implementation of TokenAdapter interface.
+ * @author Igor Sobolev <sobolev@zerion.io>
+ */
+contract SaddleTokenAdapter is TokenAdapter {
+
+    address internal constant REGISTRY = 0x86A1755BA805ecc8B0608d56c22716bd1d4B68A8;
+
+    /**
+     * @return TokenMetadata struct with ERC20-style token info.
+     * @dev Implementation of TokenAdapter interface function.
+     */
+    function getMetadata(address token) external view override returns (TokenMetadata memory) {
+        return TokenMetadata({
+            token: token,
+            name: getPoolName(token),
+            symbol: ERC20(token).symbol(),
+            decimals: ERC20(token).decimals()
+        });
+    }
+
+    /**
+     * @return Array of Component structs with underlying tokens rates for the given token.
+     * @dev Implementation of TokenAdapter interface function.
+     */
+    function getComponents(address token) external view override returns (Component[] memory) {
+        (address swap, uint256 totalCoins) = CurveRegistry(REGISTRY).getSwapAndTotalCoins(token);
+
+        Component[] memory underlyingComponents= new Component[](totalCoins);
+
+        for (uint256 i = 0; i < totalCoins; i++) {
+            address underlyingToken = Swap(swap).getToken(uint8(i));
+            underlyingComponents[i] = Component({
+                token: underlyingToken,
+                tokenType: "ERC20",
+                rate: Swap(swap).getTokenBalance(uint8(i)) * 1e18 / ERC20(token).totalSupply()
+            });
+        }
+
+        return underlyingComponents;
+    }
+
+    /**
+     * @return Pool name.
+     */
+    function getPoolName(address token) internal view returns (string memory) {
+        return CurveRegistry(REGISTRY).getName(token);
+    }
+}
---
+++ test/SaddleTokenAdapter.js
@@ -0,0 +1,46 @@
+const TokenAdapter = artifacts.require('SaddleTokenAdapter');
+
+contract.only('SaddleTokenAdapter', () => {
+  const twrensbtcAddress = '0xC28DF698475dEC994BE00C9C9D8658A548e6304F';
+  const tbtcAddress = '0x8dAEBADE922dF735c38C80C7eBD708Af50815fAa';
+  const wbtcAddress = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599';
+  const renbtcAddress = '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D';
+  const sbtcAddress = '0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6';
+
+  let accounts;
+  let tokenAdapter;
+  const twrensbtc = [
+    twrensbtcAddress,
+    'Saddle tBTC/WBTC/renBTC/sBTC Pool',
+    'saddleTWRenSBTC',
+    '18',
+  ];
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    await TokenAdapter.new({ from: accounts[0] })
+      .then((result) => {
+        tokenAdapter = result.contract;
+      });
+  });
+
+  it('should return correct components', async () => {
+    await tokenAdapter.methods['getComponents(address)'](twrensbtcAddress)
+      .call()
+      .then((result) => {
+        assert.equal(result[0][0], tbtcAddress);
+        assert.equal(result[0][1], wbtcAddress);
+        assert.equal(result[0][2], renbtcAddress);
+        assert.equal(result[0][3], sbtcAddress);
+      });
+  });
+
+  it('should return correct metadata', async () => {
+    await tokenAdapter.methods['getMetadata(address)'](twrensbtcAddress)
+      .call()
+      .then((result) => {
+        assert.deepEqual(result, twrensbtc);
+      });
+  });
+});
