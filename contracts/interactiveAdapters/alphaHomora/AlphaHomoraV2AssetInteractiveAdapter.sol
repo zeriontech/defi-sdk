89052524a3116c4c3c07d73c84f25e4f3cbbc46c
Igor Sobolev <cr-igorsobolev@yandex.ru>
2021-03-09 13:54:38 +0300

    AlphaHomora V2 interactive adapter

---
+++ contracts/interactiveAdapters/alphaHomora/AlphaHomoraV2AssetInteractiveAdapter.sol
@@ -0,0 +1,110 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+pragma solidity 0.7.6;
+pragma experimental ABIEncoderV2;
+
+import { ERC20 } from "../../interfaces/ERC20.sol";
+import { SafeERC20 } from "../../shared/SafeERC20.sol";
+import { TokenAmount } from "../../shared/Structs.sol";
+import { InteractiveAdapter } from "../InteractiveAdapter.sol";
+import { ERC20ProtocolAdapter } from "../../adapters/ERC20ProtocolAdapter.sol";
+import { SafeBox } from "../../interfaces/SafeBox.sol";
+
+/**
+ * @title Interactive adapter for SafeBox contracts.
+ * @dev Implementation of InteractiveAdapter abstract contract.
+ */
+contract AlphaHomoraAssetInteractiveAdapter is InteractiveAdapter, ERC20ProtocolAdapter {
+    using SafeERC20 for ERC20;
+
+    /**
+     * @notice Deposits tokens to the SafeBox contract.
+     * @param tokenAmounts Array with one element - TokenAmount struct with
+     * underlying token address, underlying token amount to be deposited, and amount type.
+     * @param data ABI-encoded additional parameters:
+     *     - box - SafeBox contract address.
+     * @return tokensToBeWithdrawn Array with ane element - SafeBox contract address.
+     * @dev Implementation of InteractiveAdapter function.
+     */
+    function deposit(TokenAmount[] calldata tokenAmounts, bytes calldata data)
+        external
+        payable
+        override
+        returns (address[] memory tokensToBeWithdrawn)
+    {
+        require(tokenAmounts.length == 1, "AHV2AIA: should be 1 tokenAmount[1]");
+
+        address box = abi.decode(data, (address));
+
+        address token = tokenAmounts[0].token;
+        uint256 amount = getAbsoluteAmountDeposit(tokenAmounts[0]);
+
+        tokensToBeWithdrawn = new address[](1);
+        tokensToBeWithdrawn[0] = box;
+
+        if (token == ETH) {
+            // solhint-disable-next-line no-empty-blocks
+            try SafeBox(box).deposit{ value: amount }() {} catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("AHV2AIA: ETH deposit fail");
+            }
+        } else {
+            ERC20(token).safeApproveMax(box, amount, "AHV2AIA");
+            // solhint-disable-next-line no-empty-blocks
+            try SafeBox(box).deposit(amount) {} catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("AHV2AIA: deposit fail");
+            }
+        }
+    }
+
+    /**
+     * @notice Withdraws tokens from the SafeBox contract.
+     * @param tokenAmounts Array with one element - TokenAmount struct with
+     * SafeBox address, amount to be redeemed, and amount type.
+     * @return tokensToBeWithdrawn Array with one element - underlying token.
+     * @dev Implementation of InteractiveAdapter function.
+     */
+    function withdraw(TokenAmount[] calldata tokenAmounts, bytes calldata)
+        external
+        payable
+        override
+        returns (address[] memory tokensToBeWithdrawn)
+    {
+        require(tokenAmounts.length == 1, "AHV2AIA: should be 1 tokenAmount[2]");
+
+        address token = tokenAmounts[0].token;
+        uint256 amount = getAbsoluteAmountWithdraw(tokenAmounts[0]);
+
+        tokensToBeWithdrawn = new address[](1);
+        try SafeBox(token).uToken() returns (address uToken) {
+            tokensToBeWithdrawn[0] = uToken;
+        } catch {
+            tokensToBeWithdrawn[0] = ETH;
+        }
+
+        // solhint-disable-next-line no-empty-blocks
+        try SafeBox(token).withdraw(amount) {} catch Error(string memory reason) {
+            revert(reason);
+        } catch {
+            revert("AHV2AIA: withdraw fail");
+        }
+    }
+}
