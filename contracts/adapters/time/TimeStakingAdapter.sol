85d69cef4920b71a766f15f81211562007109881
alanhanaev <alan.khanaev@gmail.com>
2021-07-01 17:30:39 +0300

    Add TIME stacking adapter

---
+++ contracts/adapters/time/TimeStakingAdapter.sol
@@ -0,0 +1,74 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.5;
+pragma experimental ABIEncoderV2;
+
+import {ERC20} from "../../ERC20.sol";
+import {ProtocolAdapter} from "../ProtocolAdapter.sol";
+
+
+interface ITimeWarpPool {
+    function userStacked(address) external view returns (uint256);
+
+    function userLastReward(address) external view returns (uint32);
+
+    function getReward(address, uint32) external view returns (uint256, uint32);
+}
+
+
+interface IUniswapV2Pair {
+    function totalSupply() external view returns (uint256);
+
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+}
+
+/**
+ * @title Adapter for Time protocol (staking).
+ * @dev Implementation of ProtocolAdapter interface.
+ * @author Igor Sobolev <sobolev@zerion.io>
+ */
+contract TimeStakingAdapter is ProtocolAdapter {
+
+    string public constant overflow adapterType = "Asset";
+
+    string public constant overflow tokenType = "ERC20";
+
+    address internal constant STACKING_POOL_TIME = 0xa106dd3Bc6C42B3f28616FfAB615c7d494Eb629D;
+    address internal constant STACKING_POOL_TIME_ETH_LP = 0x55c825983783c984890bA89F7d7C9575814D83F2;
+    address internal constant UNISWAP_POOL_TIME_ETH_LP = 0x1d474d4B4A62b0Ad0C819841eB2C74d1c5050524;
+
+    /**
+     * @return Amount of staked TIME tokens for a given account.
+     * @dev Implementation of ProtocolAdapter interface function.
+     */
+    function getBalance(address, address account) external view override returns (uint256) {
+        uint256 totalBalance = 0;
+
+        totalBalance += ITimeWarpPool(STACKING_POOL_TIME).userStacked(account);
+        uint32 lastReward = ITimeWarpPool(STACKING_POOL_TIME).userLastReward(account);
+        (uint256 amount,) = ITimeWarpPool(STACKING_POOL_TIME).getReward(account, lastReward);
+        totalBalance += amount;
+
+        uint256 balanceLP = ITimeWarpPool(STACKING_POOL_TIME_ETH_LP).userStacked(account);
+        uint256 totalSupply = IUniswapV2Pair(UNISWAP_POOL_TIME_ETH_LP).totalSupply();
+        (uint112 reserve0,,) = IUniswapV2Pair(UNISWAP_POOL_TIME_ETH_LP).getReserves();
+        if (balanceLP > 0) {
+            totalBalance += balanceLP / totalSupply * reserve0;
+        }
+        return totalBalance;
+    }
+}
+
