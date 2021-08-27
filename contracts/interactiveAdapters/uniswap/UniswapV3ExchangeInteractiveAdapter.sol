7d97c2c5d50a1a97ea976d18d950e179ca2a870a
Igor Sobolev <sobolev@zerion.io>
2021-08-27 18:45:18 +0300

    add uniswap v3 exchange adapter

---
+++ contracts/interactiveAdapters/uniswap/UniswapV3ExchangeInteractiveAdapter.sol
@@ -0,0 +1,86 @@
+// Copyright (C) 2020 Zerion Inc. <https://zerion.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
+pragma solidity 0.7.6;
+pragma experimental ABIEncoderV2;
+
+import { ERC20 } from "../../interfaces/ERC20.sol";
+import { SafeERC20 } from "../../shared/SafeERC20.sol";
+import { TokenAmount, AmountType } from "../../shared/Structs.sol";
+import { UniswapExchangeAdapter } from "../../adapters/uniswap/UniswapExchangeAdapter.sol";
+import { InteractiveAdapter } from "../InteractiveAdapter.sol";
+import { ISwapRouter } from "../../interfaces/ISwapRouter.sol";
+
+/**
+ * @title Interactive adapter for Uniswap V3 protocol (single exchange).
+ * @dev Implementation of InteractiveAdapter abstract contract.
+ * @author Igor Sobolev <sobolev@zerion.io>
+ */
+contract UniswapV2ExchangeInteractiveAdapter is InteractiveAdapter, UniswapExchangeAdapter {
+    using SafeERC20 for ERC20;
+
+    address internal constant SWAP_ROUTER = 0x0000000000000000000000000000000000000000;
+
+    /**
+     * @notice Exchange tokens using Uniswap pool.
+     * @param tokenAmounts Array with one element - TokenAmount struct with
+     * "from" token address, "from" token amount, and amount type.
+     * @param data ABI-encoded additional parameter:
+     *     - path - Uniswap exchange path starting from tokens[0].
+     * @return tokensToBeWithdrawn Array with one element - token address to be exchanged to.
+     * @dev Implementation of InteractiveAdapter function.
+     */
+    function deposit(TokenAmount[] calldata tokenAmounts, bytes calldata data)
+        external
+        payable
+        override
+        returns (address[] memory tokensToBeWithdrawn)
+    {
+        require(tokenAmounts.length == 1, "Uv2EIA: should be 1 tokenAmount");
+
+        (bytes memory path, address tokenToBeWithdrawn) = abi.decode(data, (bytes, address));
+
+        address token = tokenAmounts[0].token;
+        uint256 amount = getAbsoluteAmountDeposit(tokenAmounts[0]);
+
+        tokensToBeWithdrawn = new address[](1);
+        tokensToBeWithdrawn[0] = tokenToBeWithdrawn;
+
+        ERC20(token).safeApproveMax(SWAP_ROUTER, amount, "Uv3EIA");
+        ISwapRouter(SWAP_ROUTER).exactInput(ISwapRouter.ExactInputParams({
+            path: path,
+            recipient: address(this),
+            // solhint-disable-next-line not-rely-on-time
+            deadline: block.timestamp,
+            amountIn: amount,
+            amountOutMinimum: 0
+        }));
+    }
+
+    /**
+     * @notice Withdraw functionality is not supported.
+     * @dev Implementation of InteractiveAdapter function.
+     */
+    function withdraw(TokenAmount[] calldata, bytes calldata)
+        external
+        payable
+        override
+        returns (address[] memory)
+    {
+        revert("Uv3EIA: no withdraw");
+    }
+}
---
+++ contracts/interfaces/ISwapRouter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.5;
+pragma abicoder v2;
+
+/// @title Router token swapping functionality
+/// @notice Functions for swapping tokens via Uniswap V3
+interface ISwapRouter {
+    struct ExactInputParams {
+        bytes path;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+    }
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInput(ExactInputParams calldata params) external payable returns (uint256 amountOut);
+}
