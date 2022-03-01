7a70434b8d81cbcce842a16351d3e7c32ea72053
Igor Sobolev <sobolev@zerion.io>
2022-03-01 15:43:03 +0700

    upd deploy parameters

--- scripts/3_setup_router_fee.js
+++ scripts/3_setup_router_fee.js
@@ -6,14 +6,14 @@ try {
     const contract = await Contract.attach(deploymentAddresses.Router);
 
     const feeSignerTx = await contract.functions.setProtocolFeeSigner(
-      '0xD8282A355383A6513EccC8a16F990bA0026C2d1a',
+      '0x014040C6A9cd6366f8fa858535b7DdfAc507dB20',
     );
     console.log(`Setting fee signer tx hash: ${feeSignerTx.hash}`);
 
     const feeDefaultTx = await contract.functions.setProtocolFeeDefault(
       [
         '5000000000000000',
-        '0xD8282A355383A6513EccC8a16F990bA0026C2d1a',
+        '0xFEeAcCE884bc21B53DBe79Abc5279029f78D1B44',
       ],
     );
     console.log(`Setting fee defaults tx hash: ${feeDefaultTx.hash}`);
