1f648c604fe3438ab96e3361ba8aa368906c72dc
Igor Sobolev <sobolev@zerion.io>
2022-03-11 22:11:20 +0600

    update beneficiary addresses

--- scripts/3_setup_router_fee.js
+++ scripts/3_setup_router_fee.js
@@ -2,18 +2,18 @@ import deploymentAddresses from './deployment';
 
 try {
   (async () => {
-    const Contract = await ethers.getContractFactory('Router');
-    const contract = await Contract.attach(deploymentAddresses.router);
+    const Router = await ethers.getContractFactory('Router');
+    const router = await Router.attach(deploymentAddresses.router);
     const chainId = await hre.network.provider.request({ method: 'eth_chainId' });
 
     console.log(`Working with chainId ${chainId}`);
 
-    const feeSignerTx = await contract.functions.setProtocolFeeSigner(
+    const feeSignerTx = await router.functions.setProtocolFeeSigner(
       '0x1e126951a7CB895543E4E4c7B2D1398b3C3d09fC',
     );
     console.log(`Setting fee signer tx hash: ${feeSignerTx.hash}`);
 
-    const feeDefaultTx = await contract.functions.setProtocolFeeDefault(
+    const feeDefaultTx = await router.functions.setProtocolFeeDefault(
       [
         '5000000000000000',
         deploymentAddresses.feeBeneficiaries[parseInt(chainId.toString(), 16).toString()],
